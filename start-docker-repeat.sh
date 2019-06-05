b40984207fb08d48fa359c5e3cab208832aa7039
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2019-06-05 18:25:19 +0100

    Docker repeat

---
+++ start-docker-repeat.sh
@@ -0,0 +1,4 @@
+docker run -d                                          \
+	-v test-key-vol:/home/bot/config                   \
+	-v repeat-images:/home/bot/images                  \
+	repeat-twitterbot
