f3b96de68d28f486c80b7dcb4bb15ae0f7e5040e
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2019-06-08 19:46:43 +0100

    Use repeat key instead of test key

--- docker-run-repeat.sh
+++ docker-run-repeat.sh
@@ -1,4 +1,4 @@
 docker run -d                                          \
-	-v test-key-vol:/home/bot/config                   \
+	-v repeat-key-vol:/home/bot/config                   \
 	-v repeat-images:/home/bot/images                  \
 	repeat-twitterbot
